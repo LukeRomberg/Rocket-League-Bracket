79154a9fe4834f5627264a0c8723ef79932de0a0
Luke Romberg <Lukerromberg@gmail.com>
2017-09-13 18:20:34 -0600

    Made a working prototype, no CSS and incomplete JS

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var form = document.getElementsByClassName('playerNumberButton')[0];
 var playerNames = document.getElementsByClassName('playerNames')[0];
 var bracket = document.getElementById('bracket')
+
 form.addEventListener('submit', function(event){
     event.preventDefault()
     var playerNumber = event.target.elements.playerNumberInput.valueAsNumber
@@ -41,7 +42,7 @@ playerNames.addEventListener('submit', (event)=>{
     for (var i=0;i<event.target.elements.length-1;i++){
         var li = document.createElement('button')
         li.setAttribute('id','teamNumber' + [i])
-        li.setAttribute('id','team')
+        li.setAttribute('class','team')
         li.innerText = event.target.elements[i].value
         bracket.append(li)
         players.push(event.target.elements[i].value)
@@ -49,32 +50,40 @@ playerNames.addEventListener('submit', (event)=>{
     }
     hide(playerNames)
 })
+var data = document.getElementById('data')
+var playerForm = document.getElementById('playerForm')
 
-// Temp code
-// function deselect(e) {
-//   $('.pop').slideFadeToggle(function() {
-//     e.removeClass('selected');
-//   });
-// }
-//
-// $(function() {
-//   $('#team').on('click', function() {
-//     if($(this).hasClass('selected')) {
-//       deselect($(this));
-//     } else {
-//       $(this).addClass('selected');
-//       $('.pop').slideFadeToggle();
-//     }
-//     return false;
-//   });
-//
-//   $('.close').on('click', function() {
-//     deselect($('#contact'));
-//     return false;
-//   });
-// });
-//
-// $.fn.slideFadeToggle = function(easing, callback) {
-//   return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
-// };
-// //end code
+bracket.addEventListener('click', function(event){
+    var formSubmit = document.createElement('button')
+    var goals = document.createElement('input')
+    var assists = document.createElement('input')
+    var saves = document.createElement('input')
+    var shots = document.createElement('input')
+    var formLabel = document.createElement('h3')
+    var newForm = document.createElement('form')
+        goals.innerText = 'Goals'
+        goals.setAttribute('placeholder', 'Goals')
+        assists.innerText = 'Assists'
+        assists.setAttribute('placeholder', 'Assists')
+        saves.innerText = 'Saves'
+        saves.setAttribute('placeholder', 'Saves')
+        shots.innerText = 'Shots'
+        shots.setAttribute('placeholder', 'Shots')
+        formSubmit.setAttribute('type', 'submit')
+        formSubmit.innerText = 'Submit'
+        formLabel.innerText = event.target.innerText
+        newForm.append(formLabel, goals, assists, saves, shots, formSubmit)
+        playerForm.append(newForm)
+})
+playerForm.addEventListener('submit', function(event){
+    event.preventDefault()
+    for (var i = 0; i < event.target.elements.length; i++) {
+        if (i = 0){
+            console.log(event.target.children[i].innerText);
+        }else {
+        console.log(event.target.elements[i].attributes[0].nodeValue + ' '+ event.target.elements[i].value)
+        }
+    }
+}
+    playerForm.innerHTML = ''
+})
