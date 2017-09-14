e7f816a7c7d68c94162e75ee9b873475af44eda7
Luke Romberg <Lukerromberg@gmail.com>
2017-09-14 12:27:02 -0600

    Added CSS, working on JS

--- public/script.js
+++ public/script.js
@@ -1,7 +1,14 @@
 var form = document.getElementsByClassName('playerNumberButton')[0];
 var playerNames = document.getElementsByClassName('playerNames')[0];
 var bracket = document.getElementById('bracket')
-
+var bracket1 = document.getElementById('bracket1')
+var bracket2 = document.getElementById('bracket2')
+var bracket3 = document.getElementById('bracket3')
+var bracket4 = document.getElementById('bracket4')
+var bracket5 = document.getElementById('bracket5')
+var bracket6 = document.getElementById('bracket6')
+var bracket7 = document.getElementById('bracket7')
+var bracket8 = document.getElementById('bracket8')
 form.addEventListener('submit', function(event){
     event.preventDefault()
     var playerNumber = event.target.elements.playerNumberInput.valueAsNumber
@@ -27,6 +34,7 @@ function populateTeamNames(number){
       }
     let button = document.createElement('button')
     button.setAttribute('type','submit')
+    button.setAttribute('class','btn btn-dark')
     button.innerText = 'Submit'
     playerNames.append(button)
 }
@@ -46,19 +54,30 @@ playerNames.addEventListener('submit', (event)=>{
         li.innerText = event.target.elements[i].value
         bracket.append(li)
         players.push(event.target.elements[i].value)
-
     }
+    // let button = document.createElement('button')
+    // button.setAttribute('type','submit')
+    // button.setAttribute('class','btn btn-dark')
+    // button.innerText = 'Next Round'
+    // bracket.append(button)
     hide(playerNames)
 })
+// bracket.addEventListener('submit', function(event){
+//     event.preventDefault()
+//
+// })
 var data = document.getElementById('data')
 var playerForm = document.getElementById('playerForm')
 
 bracket.addEventListener('click', function(event){
+    playerForm.innerHTML = ''
     var formSubmit = document.createElement('button')
     var goals = document.createElement('input')
     var assists = document.createElement('input')
     var saves = document.createElement('input')
     var shots = document.createElement('input')
+    var winnerLabel = document.createElement('label')
+    var winner = document.createElement('input')
     var formLabel = document.createElement('h3')
     var newForm = document.createElement('form')
         goals.innerText = 'Goals'
@@ -69,21 +88,26 @@ bracket.addEventListener('click', function(event){
         saves.setAttribute('placeholder', 'Saves')
         shots.innerText = 'Shots'
         shots.setAttribute('placeholder', 'Shots')
+        winnerLabel.innerText = 'Move ' + event.target.innerText + ' on?'
+        winner.setAttribute('type', 'checkbox')
         formSubmit.setAttribute('type', 'submit')
         formSubmit.innerText = 'Submit'
         formLabel.innerText = event.target.innerText
-        newForm.append(formLabel, goals, assists, saves, shots, formSubmit)
+        newForm.append(formLabel, goals, assists, saves, shots, winnerLabel, winner, formSubmit)
         playerForm.append(newForm)
 })
 playerForm.addEventListener('submit', function(event){
     event.preventDefault()
-    for (var i = 0; i < event.target.elements.length; i++) {
-        if (i = 0){
-            console.log(event.target.children[i].innerText);
+    var counter = 0
+    while (counter < event.target.elements.length -2){
+        if (counter === 0){
+            console.log(event.target.children[counter].innerText)
+            console.log(event.target.elements[counter].attributes[0].nodeValue + ' '+ event.target.elements[counter].value)
         }else {
-        console.log(event.target.elements[i].attributes[0].nodeValue + ' '+ event.target.elements[i].value)
+        console.log(event.target.elements[counter].attributes[0].nodeValue + ' '+ event.target.elements[counter].value)
         }
+        counter++
     }
-}
+    console.log(event);
     playerForm.innerHTML = ''
 })
